c8458d04aa827ee371646003bf1bfb5b07405f72
nathan099-dev <tonininathan8@gmail.com>
2023-01-27 17:50:43 -0300

    modificações no site

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -3,7 +3,6 @@ const nome = document.querySelector('#name');
 const tel = document.querySelector('#telefone');
 const email = document.querySelector('#email');
 
-
 const btn = document.querySelector('button');
 
 btn.addEventListener('submit', function validar(event){
@@ -11,5 +10,6 @@ btn.addEventListener('submit', function validar(event){
 
     if (nome  == null) {
         window.alert('Campo nome, INVÁLIDO')
+        
     }
 })
